1a3bccfd7e1278bac551328216f33a3aa647d089
Addani Raghavender <raghavender1502@gmail.com>
2023-09-14 12:00:09 +0530

    added hacker target folder

---
+++ Hackertarget/test.py
@@ -0,0 +1,24 @@
+import requests
+
+def analyticslookup(domain):
+    res = requests.get('https://api.hackertarget.com/analyticslookup/?q=' + domain)
+    response = res.text.splitlines()
+
+    # domains = []
+    # user_agents = []
+
+    # for line in response:
+    #     domain, user_agent = line.split(',')
+    #     domains.append(domain.strip())
+    #     user_agents.append(user_agent.strip())
+
+    payload = {
+        "Domain": domain,
+        "results": response
+        # "DomainsList": domains,
+        # "UserAgentsList": user_agents
+    }
+    return payload
+
+# Example usage:
+print(analyticslookup('elch.org'))
