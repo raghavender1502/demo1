6576d39261b9d81202c4f1072255a05b1cf56d04
Addani Raghavender <raghavender1502@gmail.com>
2023-09-22 18:39:15 +0530

    deleted

--- IOCFeedSpotAdd/demo.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api.iocparser.com"
-
-payload = {}
-headers= {}
-
-response = requests.request("GET", url, headers=headers, data = payload)
-print(response.text.encode('utf8'))
